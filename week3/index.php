36a19021215ebc81de71c68afaa8b78af444d5da
Marcus <marcus.jeong96@gmail.com>
2024-09-18 16:45:57 -0400

    finished time assignment

--- week3/index.php
+++ week3/index.php
@@ -9,18 +9,24 @@
 <body>
     <?php 
     // set up a variable and change it's value based on the time given
-    $time = rand(5, 21);
+    // $time = rand(5, 21);
+    //  $comp = $time;
+    date_default_timezone_set("America/Toronto");
+    $time = date("h:ia");
+    $comp = date('H');
+    $timeBreafast = 9;
+    $timeLunch = 18;
+
     $meal;
-    $timeAmPm;
     $breakfast = "Bananas, Apples, and Oats.";
     $lunch = "Fish, Chicken, and Vegetables.";
     $dinner = "Steak, Carrots, and Broccoli.";
     $mealServed;
 
-    if ($time < 9) {
+    if ($comp <= $timeBreafast) {
         $meal = "breakfast";
         $mealServed = $breakfast;
-    } elseif ($time < 14) {
+    } elseif ($comp <= $timeLunch) {
         $meal = "lunch";
         $mealServed = $lunch;
     } else {
@@ -28,13 +34,7 @@
         $mealServed = $dinner;
     }
 
-    if ($time > 12) {
-        $timeAmPm = ($time - 12) . 'pm';
-    } else {
-        $timeAmPm =  $time . 'am';
-    }
-
-    echo '<h1>The time is ' . $timeAmPm . "</h1>";
+    echo '<h1>The time is ' . $time . "</h1>";
     echo '<h2>The animals should eat ' . $meal .'.</h2>';
     echo '<p>This includes ' . $mealServed . '</p>';
     ?>
