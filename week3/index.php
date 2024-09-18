2e94e186f7dc79a5d3074fcca2231351c9022d8a
Marcus <marcus.jeong96@gmail.com>
2024-09-18 16:20:01 -0400

    week 3 assignment initial commit

--- week3/index.php
+++ week3/index.php
@@ -7,10 +7,36 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    <!-- zoo meal timer --> 
+    <?php 
+    // set up a variable and change it's value based on the time given
+    $time = rand(5, 21);
+    $meal;
+    $timeAmPm;
+    $breakfast = "Bananas, Apples, and Oats.";
+    $lunch = "Fish, Chicken, and Vegetables.";
+    $dinner = "Steak, Carrots, and Broccoli.";
+    $mealServed;
 
+    if ($time < 9) {
+        $meal = "breakfast";
+        $mealServed = $breakfast;
+    } elseif ($time < 14) {
+        $meal = "lunch";
+        $mealServed = $lunch;
+    } else {
+        $meal = "dinner";
+        $mealServed = $dinner;
+    }
 
+    if ($time > 12) {
+        $timeAmPm = ($time - 12) . 'pm';
+    } else {
+        $timeAmPm =  $time . 'am';
+    }
+
+    echo '<h1>The time is ' . $timeAmPm . "</h1>";
+    echo '<h2>The animals should eat ' . $meal .'.</h2>';
+    echo '<p>This includes ' . $mealServed . '</p>';
     ?>
 </body>
 </html>
